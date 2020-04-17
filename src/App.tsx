9a5f23d089d32461c3a4cdf17d143c64c0103201
DevAbas <abas.memmedov.94@gmail.com>
2020-04-18 00:12:28 +0400

    Completed Banner and Header sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './components/Header';
+import Banner from './components/Banner';
 import './styles/App.scss';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    let vh: number = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, []);
+
   return (
     <div className='App'>
       <Header />
+      <Banner />
     </div>
   );
 };
---
+++ src/components/Banner.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ReactComponent as RightArrow } from '../assets/arrow-right.svg';
+
+const Banner: React.FC = () => {
+  return (
+    <section className='main'>
+      <div className='container'>
+        <div className='row'>
+          <h2>
+            <div className='line'>
+              <span>Creating unique brand is</span>
+            </div>
+            <div className='line'>
+              <span>what we do.</span>
+            </div>
+          </h2>
+          <div className='btn-row'>
+            <a href='/'>
+              More about us <RightArrow />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
