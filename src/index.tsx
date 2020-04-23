79b3cda8921c0c7ef4ed8e8bd327636b9be13069
DevAbas <abas.memmedov.94@gmail.com>
2020-04-23 14:54:12 +0400

    basename added to BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Router>
+  <Router basename={`${process.env.PUBLIC_URL}`}>
     <App />
   </Router>,
   document.getElementById('root')
