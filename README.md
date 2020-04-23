de6f39a29c1aed1f9d5409a3a324e666ea570026
DevAbas <abas.memmedov.94@gmail.com>
2020-04-23 14:34:09 +0400

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Available Scripts
-
-In the project directory, you can run:
-
-### `yarn start`
---
+++ precache-manifest.bf069537a0c9abbff428e6be66163bfd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0442d659064ba4ff3ddccb6c2cbec445",
+    "url": "/agency/index.html"
+  },
+  {
+    "revision": "94a50c4459a45f55cc3d",
+    "url": "/agency/static/css/main.e63124bc.chunk.css"
+  },
+  {
+    "revision": "0e447c28cc5e97117fc1",
+    "url": "/agency/static/js/2.2aac8443.chunk.js"
+  },
+  {
+    "revision": "723894b771c1fe011b0df55ec646ea2d",
+    "url": "/agency/static/js/2.2aac8443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a50c4459a45f55cc3d",
+    "url": "/agency/static/js/main.48d664f2.chunk.js"
+  },
+  {
+    "revision": "2ce38c1205cb435ad9f9",
+    "url": "/agency/static/js/runtime-main.38e4778b.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/agency/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/agency/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/agency/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/agency/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "24e8d8f29296438b099443772c822a1f",
+    "url": "/agency/static/media/up-arrow-circle.24e8d8f2.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/agency/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
